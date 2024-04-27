7e3e53354fd74be03e8bc9756deb97357a149659
Faixan6190 <faixan6190@gmail.com>
2024-04-27 14:39:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,118 @@
+// import express from "express";
+
+// const app = express();
+// const PORT = 5000;
+
+// app.get("/", (request, response) => {
+//   response.send("Hello World , Hello Faizan");
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`server running on http://localhost:${PORT}`)
+// );
+
+// import express from "express";
+
+// const app = express();
+// const PORT = 5000;
+
+// app.get("/", (request, response) => {
+//   response.send("Hello Thursday , Hello Friday");
+// });
+// app.get("/contact", (request, response) => {
+//   response.send("Hello Contact");
+// });
+// app.get("/about", (request, response) => {
+//   response.send("Hello About");
+// });
+// app.get("/overview", (request, response) => {
+//   response.send("Hello Overview");
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`server running on http://localhost:${PORT}`)
+// );
+
+// import express from "express";
+// import { product } from "./product.js";
+// import { user } from "./user.js";
+// import { temp } from "./temp.js";
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.get("/", (request, response) => {
+//   response.send("Hello Index");
+// });
+
+// app.get("/product", (request, response) => {
+//   response.send(product);
+// });
+
+// app.use("/user", (request, response) => {
+//   response.send(user);
+// });
+
+// app.use((request, response, next) => {
+//   console.log("hit every request");
+//   const isAuth = true;
+//   if (!isAuth) {
+//     response.send("UNAUTH USER");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((request, response, next) => {
+//   console.log("Hello I'm Middleware");
+//   // return response.json({
+//   //   message: "Not Sending A Request Some Security Reason",
+//   // });
+//   next();
+// });
+
+// app.get("/temp", (request, response) => {
+//   response.send(temp);
+// });
+
+// app.use((req, resp, next) => {
+//   console.log("hit every request");
+//   const isAuth = true;
+//   if (!Auth) {
+//     resp.json({
+//       data: null,
+//       message: "UnAuth User",
+//     });
+//   } else {
+//     next();
+//   }
+// });
+
+import express from "express";
+import { product } from "./product.js";
+import { user } from "./user.js";
+import { temp } from "./temp.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// const authMiddleWare = (request, response, next) => {
+//   console.log("hit every request");
+//   const isAuth = true;
+//   if (!isAuth) {
+//     response.json({
+//       data: null,
+//       message: "not valid",
+//     });
+//   } else {
+//     next();
+//   }
+// };
+
+app.get("/", authMiddleWare, (request, response) => {
+  response.send("TEsting Middleware");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
---
+++ temp.js
@@ -0,0 +1,17 @@
+export const temp = [
+  {
+    id: "1",
+    name: "Faizan",
+    class: 10,
+  },
+  {
+    id: "2",
+    name: "Ahmed",
+    class: 11,
+  },
+  {
+    id: "3",
+    name: "Faiq",
+    class: 18,
+  },
+];
