9d5038a87ed5683405c855a187bee2b2081434d5
Faixan6190 <faixan6190@gmail.com>
2024-06-29 14:32:42 +0500

    resolving

--- index.js
+++ index.js
@@ -337,7 +337,7 @@ app.get("/", (request, respone) => {
 });
 
 app.post("/user", (request, response) => {
-  users.push(request.body.id);
+  users.push(request.body);
   response.send({ user: request.body, message: "User added successfully" });
 });
 
