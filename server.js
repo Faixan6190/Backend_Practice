b33383e5777d99af2a3ed6327b571d3d65c68d77
Faixan6190 <faixan6190@gmail.com>
2024-08-04 15:19:51 +0500

    new api

--- server.js
+++ server.js
@@ -7,22 +7,28 @@ const PORT = 5000;
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send(new Date().toString());
-});
+const users = [];
 
-app.get("/product", (request, response) => {
-  response.send(products);
-});
+// app.get("/", (request, response) => {
+//   response.send(new Date().toString());
+// });
 
-app.get("/products", authMiddleWare, (request, response) => {
-  console.log("request.query", request.query);
-  //   response.send(user);
+// app.get("/product", (request, response) => {
+//   response.send(products);
+// });
+
+// app.get("/products", authMiddleWare, (request, response) => {
+//   console.log("request.query", request.query);
+//   //   response.send(user);
+// });
+
+app.get("/getuser", (request, response) => {
+  response.send(users);
 });
 
 app.post("/createpost", (request, response) => {
-  console.log("request", request.body);
-  response.send({ message: "Post Request" });
+  users.push(request.body);
+  response.send({ users: request.body, message: "User added successfully" });
 });
 
 app.listen(PORT, () => console.log(`Server is Running on http://localhost:${PORT}`));
