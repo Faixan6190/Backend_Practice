d79632997e519ab211b5a38f60181701cbb94056
Faixan6190 <faixan6190@gmail.com>
2024-08-05 20:22:41 +0500

    create post api

--- controller/postController.js
+++ controller/postController.js
@@ -2,5 +2,7 @@ export const createPost = async (request, response) => {
   try {
     console.log("request", request.body);
     response.status(200).send({ status: 200 });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
