c6fd18e8c1de6ec1166a4f27ccbc798763145a4b
Faixan6190 <faixan6190@gmail.com>
2024-08-07 01:19:35 +0500

    add files

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 import UserModal from "../models/userSchema.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 export const signupUser = async (request, response) => {
   try {
@@ -7,7 +9,8 @@ export const signupUser = async (request, response) => {
     const user = await UserModal.create({ ...request.body, password });
     const data = user.toObject();
     delete data.password;
-    response.status(201).send({ status: 201, user: data });
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+    response.status(201).send({ status: 201, user: data, token });
   } catch (error) {
     response.status(500).send({ status: 500, error });
   }
@@ -16,9 +19,15 @@ export const signupUser = async (request, response) => {
 export const loginUser = async (request, response) => {
   try {
     const { email, password } = request.body;
-    const user = await UserModal.find({ email });
+    const user = await UserModal.findOne({ email });
     if (user) {
       const checkPassword = bcrypt.compareSync(password, user.password);
+      if (checkPassword) {
+        const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+        response.status(200).send({ status: 200, user, message: "Login successfully", token });
+      } else {
+        response.status(401).send({ status: 401, message: "Incorrect password" });
+      }
     } else {
       response.status(404).send({ status: 404, message: "User not found" });
     }
