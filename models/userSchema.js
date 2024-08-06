d761e47537efbc1927e86b544ed3b0fbe1ab44fb
Faixan6190 <faixan6190@gmail.com>
2024-08-06 16:53:41 +0500

    add mongoose

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,21 +1,19 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   age: Number,
-// });
-
-// const userModal = mongoose.model("user", userSchema);
-
-// export default userModal;
-
-import Joi from "joi";
-
-const userSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+const userSchema = new mongoose.Schema({
+  userName: mongoose.Schema.Types.String,
 });
 
-export { userSchema };
+const userModal = mongoose.model("user", userSchema);
+
+export default userModal;
+
+// import Joi from "joi";
+
+// const userSchema = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   password: Joi.string().required(),
+// });
+
+// export { userSchema };
