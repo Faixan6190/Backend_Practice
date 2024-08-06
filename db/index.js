d9f677010bcacee23449e594432d9b53c81e81f3
Faixan6190 <faixan6190@gmail.com>
2024-08-06 16:35:18 +0500

    add env

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
-const url = `mongodb+srv://admin:admin@cluster0.uxxmpv3.mongodb.net/`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uxxmpv3.mongodb.net/`;
 mongoose.connect(url);
 
 export default mongoose;
