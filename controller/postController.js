4b90fd3ab3c337732a021386b5373e528843a238
Faixan6190 <faixan6190@gmail.com>
2024-08-06 19:54:13 +0500

    get data from database

--- controller/postController.js
+++ controller/postController.js
@@ -3,10 +3,19 @@ import UserModal from "../models/userSchema.js";
 export const createPost = async (request, response) => {
   try {
     const user = await UserModal.create(request.body);
-    response.status(201).send({ status: 201, user });
+    const data = user.toObject();
+    delete data.password;
+    response.status(201).send({ status: 201, user: data });
   } catch (error) {
     response.status(500).send({ status: 500, error });
   }
 };
 
-export const getPost = (request, response) => {};
+export const getPost = async (request, response) => {
+  try {
+    const users = await UserModal.find();
+    response.status(200).send({ status: 200, users });
+  } catch (error) {
+    response.status(500).send({ status: 500, error });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createPost } from "../controller/postController.js";
+import { createPost, getPost } from "../controller/postController.js";
 
 const route = express.Router();
 
 route.post("/createpost", createPost);
+route.get("/getpost", getPost);
 
 export default route;
