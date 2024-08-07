44f7ee92809983a9629cbe3bb31ab7c5326dfbb0
Faixan6190 <faixan6190@gmail.com>
2024-08-07 19:58:53 +0500

    add

--- config/tokenVerification.js
+++ config/tokenVerification.js
@@ -1,12 +1,21 @@
 import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const tokenVerificaton = (request, response, next) => {
-  if (request.headers?.authorization) {
-    const token = request.headers.authorization.split(" ")[1];
-    console.log("token", token);
-    next();
-  } else {
-    response.status(402).send({ status: 402, message: "Token not provided" });
+  try {
+    if (request.headers?.authorization) {
+      const token = request.headers.authorization.split(" ")[1];
+      var decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (decoded) {
+        next();
+      } else {
+        response.status(402).send({ status: 402, message: "Token unauthorized" });
+      }
+    } else {
+      response.status(402).send({ status: 402, message: "Token not provided" });
+    }
+  } catch (error) {
+    response.status(402).send({ status: 402, message: "Token unauthorized" });
   }
 };
 
